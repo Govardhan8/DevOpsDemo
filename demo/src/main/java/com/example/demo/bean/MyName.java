fc7f52e062278f30ba6a1a778b03209aa280e91e
gova <gova@DESKTOP-L8B52D3>
2020-09-14 12:10:00 +0530

    first commit

---
+++ demo/src/main/java/com/example/demo/bean/MyName.java
@@ -0,0 +1,26 @@
+package com.example.demo.bean;
+
+public class MyName {
+	
+	private int id;
+	private String name;
+	
+	public MyName(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
---
+++ demo/src/main/java/com/example/demo/controller/RController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.bean.MyName;
+
+@RestController
+@RequestMapping("")
+public class RController {
+
+	@GetMapping("/get")
+	public MyName name()
+	{
+		MyName name=new MyName(1, "gova");
+		return name;
+	}
+}
